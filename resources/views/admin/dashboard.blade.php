d86c7e6cf7eb2aad4021d989889e04ccb77c6559
YouthDream0925 <youthdream0925@gmail.com>
2023-01-25 21:34:49 +0100

    Changed brands and models color.

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -10,14 +10,14 @@
         <div class="col-xxl-3 col-md-6 mb-5"></div>
         <div class="col-xxl-3 col-md-6 mb-5">
             <a href="{{ route('admin.dashboard') }}" class="txt-deco-none">
-                <div class="card card-raised bg-warning text-white">
+                <div class="card card-raised bg-primary text-white">
                     <div class="card-body px-4">
                         <div class="d-flex justify-content-between align-items-center mb-2">
                             <div class="me-2">
                                 <div class="display-5 text-white">{{ __('global.brands') }}</div>
                                 <div class="card-text">{{ __('global.manageBrands') }}</div>
                             </div>
-                            <div class="icon-circle bg-white-50 text-warning"><span class="material-icons">branding_watermark</span></div>
+                            <div class="icon-circle bg-white-50 text-primary"><span class="material-icons">branding_watermark</span></div>
                         </div>
                     </div>
                 </div>
@@ -25,14 +25,14 @@
         </div>
         <div class="col-xxl-3 col-md-6 mb-5">
             <a href="{{ route('admin.dashboard') }}" class="txt-deco-none">
-                <div class="card card-raised bg-secondary text-white">
+                <div class="card card-raised bg-info text-white">
                     <div class="card-body px-4">
                         <div class="d-flex justify-content-between align-items-center mb-2">
                             <div class="me-2">
                                 <div class="display-5 text-white">{{ __('global.models') }}</div>
                                 <div class="card-text">{{ __('global.manageModels') }}</div>
                             </div>
-                            <div class="icon-circle bg-white-50 text-secondary"><span class="material-icons">business_center</span></div>
+                            <div class="icon-circle bg-white-50 text-info"><span class="material-icons">business_center</span></div>
                         </div>
                     </div>
                 </div>
