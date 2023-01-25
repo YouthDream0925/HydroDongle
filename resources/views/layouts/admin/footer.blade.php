66cb6c322dcd2d08cecc92dd9537d038270fa890
dreamwork <vexit.two@gmail.com>
2023-01-25 06:48:22 +0100

    Test commit in VS code terminal.

--- resources/views/layouts/admin/footer.blade.php
+++ resources/views/layouts/admin/footer.blade.php
@@ -1,6 +1,6 @@
 <!-- Min-height is set inline to match the height of the drawer footer-->
 <footer class="py-4 mt-auto border-top" style="min-height: 74px">
-    <div class="container-xl px-5">
+    <div class="container-xl px-5">asdf
         <div class="d-flex flex-column flex-sm-row align-items-center justify-content-sm-between small">
             <div class="me-sm-2">Copyright &copy; Your Website 2021</div>
             <div class="d-flex ms-sm-2">
