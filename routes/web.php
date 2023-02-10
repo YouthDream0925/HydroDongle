744710a69bb74305e11b96d570a82d72868e2ff2
YouthDream0925 <youthdream0925@gmail.com>
2023-02-10 02:22:48 +0100

    Added route to show cpu and model and feature.

--- routes/web.php
+++ routes/web.php
@@ -133,6 +133,57 @@ Route::get('storage/brands/{filename}', function ($filename)
     return $response;
 });
 
+Route::get('storage/cpus/{filename}', function ($filename)
+{
+    $path = storage_path('app/public/cpus/' . $filename);
+
+    if (!File::exists($path)) {
+        abort(404);
+    }
+
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
+
+    return $response;
+});
+
+Route::get('storage/features/{filename}', function ($filename)
+{
+    $path = storage_path('app/public/features/' . $filename);
+
+    if (!File::exists($path)) {
+        abort(404);
+    }
+
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
+
+    return $response;
+});
+
+Route::get('storage/models/{filename}', function ($filename)
+{
+    $path = storage_path('app/public/models/' . $filename);
+
+    if (!File::exists($path)) {
+        abort(404);
+    }
+
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    $response = Response::make($file, 200);
+    $response->header("Content-Type", $type);
+
+    return $response;
+});
+
 Route::get('storage/slides/{filename}', function ($filename)
 {
     $path = storage_path('app/public/slides/' . $filename);
