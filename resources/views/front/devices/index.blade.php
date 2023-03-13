1857424c6df643550bcdadf665b24c13da5a3635
YouthDream0925 <youthdream0925@gmail.com>
2023-03-13 16:58:00 +0100

    Changed devices page clicking like old website.

--- resources/views/front/devices/index.blade.php
+++ resources/views/front/devices/index.blade.php
@@ -54,16 +54,13 @@
             <div id="data-wrapper" class="row ">
                 @foreach($brands as $brand)
                 <div class="col-lg-3 col-md-6">
-                    <div class="adress portfolio-carousel-single">
+                    <div class="adress portfolio-carousel-single brand-selector-front p-1">
                         @if($brand->hasMedia('brand_image'))
-                        <figure>
+                        <a href="{{ route('devices.brand', $brand->brand_id) }}">
                             <div style="width: 160px; height: 160px; margin-left: auto; margin-right: auto;">
                                 <img class="img-fluid img-responsive" src="{{ $brand->getMedia('brand_image')->first()->getUrl() }}"/>
                             </div>
-                            <figcaption>
-                                <a href="{{ route('devices.brand', $brand->brand_id) }}" class="link"><i class="la la-link"></i></a>
-                            </figcaption>
-                        </figure>
+                        </a>
                         @else
                         <img src="{{ asset('theme_front/img/ukf.png') }}" alt="img/ukf.png">
                         @endif
